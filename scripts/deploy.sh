e7b08154a77f58f02fcfe72b82439d1804538f4b
Nils Hamacher <hamacher.nils@gmail.com>
2020-10-12 11:20:54 +0200

    parsing still incorrect

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
-docker tag zutherb/monolithic-shop:latest $DOCKER_USERNAME/monolithic-shop:latest
-docker push $DOCKER_USERNAME/monolithic-shop:latest
+docker tag zutherb/monolithic-shop:latest \"$DOCKER_USERNAME\"/monolithic-shop:latest
+docker push \"$DOCKER_USERNAME\"/monolithic-shop:latest
