cbd630ef0bced7c4a438bdf879f60952e45cd425
Nils Hamacher <hamacher.nils@gmail.com>
2020-10-12 10:03:14 +0200

    change in source code to pull a existing docker file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env #!/usr/bin/env bash
 set -e
 
+docker login --username "${DOCKER_LOGIN}" --password "${DOCKER_PASSWORD}"
 docker tag zutherb/monolithic-shop:latest ${DOCKER_USERNAME}/monolithic-shop:latest
 docker push ${DOCKER_USERNAME}/monolithic-shop:latest
