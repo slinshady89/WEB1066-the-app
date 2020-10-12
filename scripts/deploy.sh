7b56dc318475794be4b2adccf6441bfcdab5a358
Nils Hamacher <hamacher.nils@gmail.com>
2020-10-12 10:34:41 +0200

    error in reading docker username

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env #!/usr/bin/env bash
 set -e
 
-docker login --username "${DOCKER_LOGIN}" --password "${DOCKER_PASSWORD}"
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
 docker tag zutherb/monolithic-shop:latest ${DOCKER_USERNAME}/monolithic-shop:latest
-docker push ${DOCKER_USERNAME}/monolithic-shop:latest
+docker push "${DOCKER_USERNAME}"/monolithic-shop:latest
