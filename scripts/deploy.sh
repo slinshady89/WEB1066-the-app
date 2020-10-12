f1a6a8e17c4ed7e0f9a04932e44f4a271184a697
Nils Hamacher <hamacher.nils@gmail.com>
2020-10-12 11:31:18 +0200

    just echo the commands

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,7 @@
 set -e
 
 echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
+echo "docker tag zutherb/monolithic-shop:latest \"${DOCKER_USERNAME}\"/monolithic-shop:latest"
 docker tag zutherb/monolithic-shop:latest "${DOCKER_USERNAME}"/monolithic-shop:latest
+echo "docker push \"${DOCKER_USERNAME}\"/monolithic-shop:latest"
 docker push "${DOCKER_USERNAME}"/monolithic-shop:latest
